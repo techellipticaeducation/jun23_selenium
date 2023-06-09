71229d6293eebc4a9e3cfe27118a90ae1992f677
vaisinghequinix <vaisingh@equinix.com>
2023-06-09 08:33:56 +0530

    day 3

---
+++ src/jun23_selenium/A.java
@@ -0,0 +1,26 @@
+package jun23_selenium;
+
+public class A {
+
+}
+
+class B{
+	
+}
+
+class C{
+	
+}
+
+class D{
+	
+}
+
+
+// in a java file, we can create n number of classes
+// 1 - only 1 public class is allowed
+// 2 - only that class can be public , whose name is same as file
+
+// when java compile the code, javac A.java
+// it will 4 classfile
+
---
+++ src/jun23_selenium/Test1.java
@@ -0,0 +1,20 @@
+package jun23_selenium;
+
+public class Test1 {
+   
+	// gettng main = main , Ctrl+space
+	// sysout, Ctrl+space [Eclipse] 
+	// sout , Ctrl+space [Intelling]
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+}
+
+
+// Package - folder
+// can we create same name file in folder ? No
+
+
+
+
+
