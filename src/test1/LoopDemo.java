a1ec17c1b56cdc384f0d54cdc081f2b48cddda7d
Teche <techellipticaeducation@gmail.com>
2023-06-21 08:10:19 +0530

    updates

---
+++ src/test1/LoopDemo.java
@@ -0,0 +1,44 @@
+package test1;
+
+public class LoopDemo {
+	public static void main(String[] args) {
+		// 3 types 
+		
+		
+//		int i = 20; // initialization
+//		while(i < 10) { // condition check
+//			System.out.println(i);
+//			i++; // increment
+//		}
+		
+		
+		// while - first while loop check the condition then execute the block
+		// airport ticket
+		
+		// do while - first execute then it check the condition - it execute atleast once
+		// bus ticket
+		
+//		int i = 20;
+//		do {
+//			System.out.println(i);
+//			i++;
+//		}while(i < 10);
+		
+		
+		
+		// for loop
+		
+//		for( int j = 0 ,k = 0 ; j < 10 && k < 10 ; j++, k++ ) {
+//			System.out.println(j);
+//		}
+		
+		
+		
+		for(    ;    ;    ) {
+			System.out.println("Hello");
+		}
+		
+		// Revisit - Doubt
+		
+	}
+}
--- src/test1/ScannerDemo.java
+++ src/test1/ScannerDemo.java
@@ -3,12 +3,36 @@ package test1;
 import java.util.Scanner;
 
 public class ScannerDemo {
-public static void main(String[] args) {
-	Scanner scan = new Scanner(System.in);
-	System.out.println("Enter a number:");
-	int k = scan.nextInt();
-	System.out.println("you have input " + k);
-	
-	
-}
+	public static void main(String[] args) {
+
+
+		// while loop
+		// item1 = 23    // 23
+		// item2 = 25    // 23+25= 48
+		// item3 = 50    // 48 + 50 = 98
+		System.out.println("Expense Calculator");
+		Scanner scan1 = new Scanner(System.in);
+		System.out.println("Enter Your Budget for today Shopping :");
+		int budget = scan1.nextInt();
+		System.out.println("Your today Budget is " + budget);
+		int k = 0;
+		int totalExpense = 0;
+		while(k != -1) {
+			Scanner scan = new Scanner(System.in);
+			System.out.println("Enter a item price:");
+			k = scan.nextInt();
+
+			if((totalExpense + k) > budget) {
+				System.out.println("You are not allowed to buy this item");
+			}else {
+
+				totalExpense = totalExpense + k;
+				System.out.println("total Expense " + totalExpense);
+				System.out.println("Amount left in pocket " + (budget -totalExpense));
+			}
+		}
+
+		System.out.println("Thank You");
+
+	}
 }
